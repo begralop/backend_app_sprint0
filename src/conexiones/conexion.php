b8119e78e709b6ec4a65ababd3ffb6f5a88d44ec
Belén Grande <bgralop@epsg.upv.es>
2021-10-18 16:31:15 +0200

    Método obtener últimas mediciones OK

--- src/conexiones/conexion.php
+++ src/conexiones/conexion.php
@@ -4,5 +4,5 @@ $mysql=new mysqli("localhost","root","","demo_logica");
 if($mysql->connect_error){
     die("Error de conexion");
 }else{
-    echo "Conexión con la base de datos correcta";
+    //echo "Conexión con la base de datos correcta";
 }
--- src/logica/obtenerTodasMediciones.php
+++ src/logica/obtenerTodasMediciones.php
@@ -1,15 +1,18 @@
 <?php
     require_once '../conexiones/conexion.php';
-    $data = file_get_contents('php://input');
-    echo $data;
-    $jsonData=json_decode($data);
 
     $query="SELECT * from datos";
     
     $resultado=$mysql->query($query);
+
+    $infoRes = array();
+
+    foreach($resultado as $row){
+        $infoRes[] = $row;
+    }
     
     if($resultado==true){
-    echo "Los datos se OBTUVIERON de forma correcta.";
+    echo json_encode($infoRes);
 }else{
     echo "Error al obtener datos.";
 }
